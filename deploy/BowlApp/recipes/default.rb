1896f6f73736818a047b22784801c070495a9929
mark <mark.hess2@gmail.com>
2016-07-25 10:07:51 -0500

    Update deployment

--- deploy/BowlApp/recipes/default.rb
+++ deploy/BowlApp/recipes/default.rb
@@ -1,6 +1,6 @@
 # update linux
 execute "update" do
-  command "apt-get update"
+    command "apt-get update"
 end
 
 # recipes for installing mongodb
@@ -10,6 +10,24 @@ include_recipe "mongodb::default"
 # python runtime version and app requirements
 python_runtime '2'
 pip_requirements '/home/mark/BowlApp/requirements.txt'
+
+# supervisor setup
+include_recipe "supervisor"
+
+cookbook_file "/etc/supervisord.conf" do
+    source "supervisord.conf"
+    mode 0644
+end
+
+# Start supervisor services
+supervisor_service "BowlApp" do
+    action :enable
+    directory "/home/mark/BowlApp"
+    command "python api.py"
+    stdout_logfile "/home/mark/BowlApp/supervisord_out.txt"
+    stdout_logfile_maxbytes "50MB"
+    redirect_stderr true
+end
 
 # # start flask app
 # execute "start app" do
